c1564e36c217848efccc79ff95128d18b66fd644
Chris Martinez <sydefekt@hotmail.com>
2015-12-27 21:18:43 -0800

    Updated *.targets import of *.nuget.props to avoid warning in MSBuild 14.0 and above

--- src/NuGetTasks/Properties/AssemblyInfo.cs
+++ src/NuGetTasks/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Reflection;
 
 [assembly: AssemblyVersion( "1.0.0.0" )]
-[assembly: AssemblyInformationalVersion( "1.0.0" )]
+[assembly: AssemblyInformationalVersion( "1.0.1" )]
 [assembly: AssemblyTitle( "More.Build.Tasks.NuGet" )]
 [assembly: AssemblyProduct( "More.Build.Tasks.NuGet" )]
 [assembly: AssemblyDescription( "Microsoft Build tasks used to create NuGet packages and their references." )]
