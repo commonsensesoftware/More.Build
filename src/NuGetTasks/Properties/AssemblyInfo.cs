aeaf85fa31cedfbe16ef1d70e24492b14ff4e419
Chris Martinez <chrimart@microsoft.com>
2017-06-16 16:26:31 -0700

    Detect and use highest available NuGet command line package. Fixes #3

--- src/NuGetTasks/Properties/AssemblyInfo.cs
+++ src/NuGetTasks/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Reflection;
 
 [assembly: AssemblyVersion( "2.0.0.0" )]
-[assembly: AssemblyInformationalVersion( "2.0.0" )]
+[assembly: AssemblyInformationalVersion( "2.0.1" )]
 [assembly: AssemblyTitle( "More.Build.Tasks.NuGet" )]
 [assembly: AssemblyProduct( "More.Build.Tasks.NuGet" )]
 [assembly: AssemblyDescription( "Microsoft Build tasks used to create NuGet packages and their references." )]
